e9f9e91b096124a631b5240c51fd37b68e721315
Lidan-Huang <huangld0314@gmail.com>
2022-01-25 15:13:14 -0800

    finished serial problem

--- serial.py
+++ serial.py
@@ -17,3 +17,16 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self, start):
+        """Create the starting number from the argument"""
+        self.start = start
+        self.original_start = 100
+    
+    def generate(self):
+        """Genreate sequential number from the start number"""
+        self.start += 1
+        return self.start - 1
+
+    def reset(self):
+        """Reset the number to the starting number"""
+        self.start = self.original_start
